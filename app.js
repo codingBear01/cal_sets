bc2103e30402bcb560a160bdd3315669376ff86d
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-19 06:02:09 +0900

    modified

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const pushUp = (cnt) => {
   const sets = {};
-  for (let i = 1; i <= Math.ceil(cnt / 2); i++) {
+  for (let i = 1; i <= cnt; i++) {
     if (!sets[i]) sets[i] = [];
 
     sets[i][0] = cnt - i + 1;
@@ -20,15 +20,5 @@ const pullUp = (cnt) => {
   return sets;
 };
 
-console.log('pushUp:', pushUp(5));
+console.log('pushUp:', pushUp(10));
 console.log('pullUp:', pullUp(5));
-
-/* 하체(스쿼트, 런지)
-하트, 클로버 = 스쿼트
-클로버 = 런지(왼)
-스페이드 = 런지(오)
-
-1~10 = 숫자 그대로
-J, K, Q = 11
-Joker = 12
-*/
